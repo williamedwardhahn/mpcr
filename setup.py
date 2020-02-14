ff57a9de6a077087b26c88ae4158cc9e968e94a7
William E Hahn <williamedwardhahn@gmail.com>
2020-02-13 23:20:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mpcrlab/importmpcr",
-    install_requires=['google_images_download'],
+    install_requires=[],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
